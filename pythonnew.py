4027bb697198239faef6d8c500115fdf315cc1d9
toqayasser <101673708+toqayasser@users.noreply.github.com>
2022-03-20 04:04:58 +0200

    Update pythonnew.py

--- pythonnew.py
+++ pythonnew.py
@@ -1,3 +1,12 @@
+#Program Name: Number scrabble game
+#Program Description: is played with the list of numbers between 1 and 9. Each player takes
+#turns picking a number from the list. Once a number has been picked, it cannot be picked
+#again. If a player has picked three numbers that add up to 15, that player wins the game.
+#However, if all the numbers are used and no player gets exactly 15, the game is a draw.
+#Last Modification Date: 12\3\2022
+#by toqa yasser
+
+
 import sys 
 num_list=[1,2,3,4,5,6,7,8,9]
 player1_list=[]
@@ -32,4 +41,7 @@ y=int(input("player2's turn ="))
 	     for j in range(i+1,len(player2_list)):
 		     for k in range(j+1,len(player2_list)):
 			     if player2_list[i]+player2_list[j]+player2_list[k]==15:
-				     print("player 2 wins")
+				     print("player 2 wins") 
+                         sys.exit()
+print("draw")                         
+
