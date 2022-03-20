4ca701de36dca16bc7272b63974a9b0dfdea5c54
toqayasser <101673708+toqayasser@users.noreply.github.com>
2022-03-20 04:00:13 +0200

    Add files via upload

---
+++ pythonnew.py
@@ -0,0 +1,35 @@
+import sys 
+num_list=[1,2,3,4,5,6,7,8,9]
+player1_list=[]
+player2_list=[]
+while len(num_list)!= 0 :
+     print("choose number:", num_list)
+     x=int(input("player1's turn="))
+     if x in num_list:
+       num_list.remove(x)
+       player1_list.append(x)  
+     else:
+         while x not in num_list : 
+               x=int(input("please choose number from the list "))
+         num_list.remove(x)
+         player1_list.append(x)  
+     for i in range (len(player1_list)):
+	     for j in range(i+1,len(player1_list)):
+		     for k in range(j+1,len(player1_list)):
+			     if player1_list[i]+player1_list[j]+player1_list[k]==15:
+				     print("player 1 wins")
+                         sys.exit()
+y=int(input("player2's turn ="))
+     if y in num_list:
+         num_list.remove(y)
+         player2_list.append(y)  
+     else:
+          while y not in num_list : 
+               y=int(input("please choose number from the list "))
+          num_list.remove(2)
+          player2_list.append(y)  
+     for i in range (len(player2_list)):
+	     for j in range(i+1,len(player2_list)):
+		     for k in range(j+1,len(player2_list)):
+			     if player2_list[i]+player2_list[j]+player2_list[k]==15:
+				     print("player 2 wins")
